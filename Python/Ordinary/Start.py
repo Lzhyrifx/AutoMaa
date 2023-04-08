8632279e19cb82c44105f1b28367fd2c4c691ee3
Lzhyrifx <2039281372@qq.com>
2023-04-08 21:01:54 +0800

    2.5.7.9

--- Python/Ordinary/Start.py
+++ Python/Ordinary/Start.py
@@ -11,7 +11,9 @@ if not test("MAA.exe", o.maa):
     time.sleep(120)
     guilog(o.file, o.log, o.log_bak)  # 日志整理
 elif test("MAA.exe", o.maa) and rougelike():
-    os.system(r"Kill.py")
+    admin()  # 提权
+    kill("MAA.exe", Ordinary.maa)  # 终止MAA进程
+    kill("HD-Player.exe", bluestack)  # 终止模拟器进程
     x = Normal()
     time.sleep(5)
     os.system(o.maa)  # 启动MAA
